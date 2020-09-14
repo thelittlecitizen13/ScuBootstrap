2c244b5b51308c87fd8b01f12b768ee2d399a178
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-14 10:42:27 +0300

    Dynamically card creation implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,139 @@
-var fluteInstrument = {
-    description: "שם של חומוסייה",
-    id: 1,
-    imagePath: "Image/halil.png",
-    instrumentOrders: null,
-    instrumentType: {id: 1, name: "נשיפה"},
-    name: "חליל",
-    price: 1500,
-    typeID: 1,
+function createInstrumentArray(){
+    var fluteInstrument = {
+        description: "שם של חומוסייה",
+        id: 1,
+        imagePath: "Image/halil.png",
+        instrumentOrders: null,
+        instrumentType: {id: 1, name: "נשיפה"},
+        name: "חליל",
+        price: 1500,
+        typeID: 1,
 
-};
-var harmonicaInstrument = {
-    description: "מפוחית בטעם של פעם",
-    id: 2,
-    imagePath: "Image/mapuhit.png",
-    instrumentOrders: null,
-    instrumentType: {id: 1, name: "נשיפה"},
-    name: "מפוחית",
-    price: 50,
-    typeID: 1,
+    };
+    var harmonicaInstrument = {
+        description: "מפוחית בטעם של פעם",
+        id: 2,
+        imagePath: "Image/mapuhit.png",
+        instrumentOrders: null,
+        instrumentType: {id: 1, name: "נשיפה"},
+        name: "מפוחית",
+        price: 50,
+        typeID: 1,
 
-};
-var tromboneInstrument = {
-    description: "סופר טרומבון",
-    id: 3,
-    imagePath: "Image/trombone.png",
-    instrumentOrders: null,
-    instrumentType: {id: 1, name: "נשיפה"},
-    name: "טרומבון",
-    price: 1000,
-    typeID: 1,
+    };
+    var tromboneInstrument = {
+        description: "סופר טרומבון",
+        id: 3,
+        imagePath: "Image/trombone.png",
+        instrumentOrders: null,
+        instrumentType: {id: 1, name: "נשיפה"},
+        name: "טרומבון",
+        price: 1000,
+        typeID: 1,
 
-};
-var trumpetInstrument = {
-    description: "חצוצרה מחשמלת",
-    id: 4,
-    imagePath: "Image/hazuzra.png",
-    instrumentOrders: null,
-    instrumentType: {id: 1, name: "נשיפה"},
-    name: "חצוצרה",
-    price: 35,
-    typeID: 1,
+    };
+    var trumpetInstrument = {
+        description: "חצוצרה מחשמלת",
+        id: 4,
+        imagePath: "Image/hazuzra.png",
+        instrumentOrders: null,
+        instrumentType: {id: 1, name: "נשיפה"},
+        name: "חצוצרה",
+        price: 35,
+        typeID: 1,
 
-};
-var violinInstrument = {
-    description: "סתם כינור (סקאם)",
-    id: 5,
-    imagePath: "",
-    instrumentOrders: null,
-    instrumentType: {id: 2, name: "פריטה"},
-    name: "כינור",
-    price: 500,
-    typeID: 2,
+    };
+    var violinInstrument = {
+        description: "סתם כינור (סקאם)",
+        id: 5,
+        imagePath: "",
+        instrumentOrders: null,
+        instrumentType: {id: 2, name: "פריטה"},
+        name: "כינור",
+        price: 500,
+        typeID: 2,
 
-};
-var clarinetInstrument = {
-    description: "הכלי של סקווידוויד",
-    id: 6,
-    imagePath: "Image/klarinet.png",
-    instrumentOrders: null,
-    instrumentType: {id: 1, name: "נשיפה"},
-    name: "קלרינט",
-    price: 2300,
-    typeID: 1,
+    };
+    var clarinetInstrument = {
+        description: "הכלי של סקווידוויד",
+        id: 6,
+        imagePath: "Image/klarinet.png",
+        instrumentOrders: null,
+        instrumentType: {id: 1, name: "נשיפה"},
+        name: "קלרינט",
+        price: 2300,
+        typeID: 1,
 
-};
+    };
+    var instruments = [fluteInstrument, harmonicaInstrument,tromboneInstrument, trumpetInstrument, clarinetInstrument, violinInstrument];
+    return instruments;
+}
 
-var instruments = [fluteInstrument, harmonicaInstrument,tromboneInstrument, trumpetInstrument, clarinetInstrument, violinInstrument];
+
+
+
+function createInstrumentDiv(instrument)
+{
+    // Main card div
+    var cardDiv = document.createElement('div');
+    cardDiv.className = 'card rounded';
+
+    // Product image
+    var cardImg = document.createElement('img');
+    cardImg.className = 'card-img-top';
+    cardImg.setAttribute('src', instrument.imagePath);
+    cardDiv.appendChild(cardImg);
+
+    // Product Text
+    var productDiv = document.createElement('div');
+    productDiv.className = 'card-body';
+
+    // Product Text - title
+    var cardHeader = document.createElement('h4');
+    cardHeader.className = 'card-title';
+    cardHeader.innerHTML = instrument.name;
+    productDiv.appendChild(cardHeader);
+
+    // Product Text - description
+    var cardDescription = document.createElement('p');
+    cardDescription.className = 'card-text';
+    cardDescription.innerHTML = instrument.description;
+    productDiv.appendChild(cardDescription);
+
+    // Product Text - price
+    var cardPrice = document.createElement('p');
+    cardPrice.className = 'card-price';
+    cardPrice.innerHTML = instrument.price;
+    productDiv.appendChild(cardPrice);
+
+    // Product Add-To-Cart - Generic
+    var addToCardButton = document.createElement('a');
+    addToCardButton.className ="btn btn-primary stretched-link"
+    addToCardButton.setAttribute('href', '#');
+    var cartImg = document.createElement('img');
+    cartImg.className = "cart-img";
+    cartImg.setAttribute('src', "Image/cart.png");
+    addToCardButton.appendChild(cartImg);
+    addToCardButton.innerHTML += "הוסף לעגלה";
+    productDiv.appendChild(addToCardButton);
+
+    cardDiv.appendChild(productDiv);
+    return cardDiv;
+}
+
+function createCards()
+{
+    console.log = "hello";
+    var instruments = createInstrumentArray();
+    var instrument;
+    var cardColumns = document.getElementById("cardColumns");
+    for (instrument of instruments)
+    {
+        console.log = "hello";
+        console.log = instrument.name;
+        var cardDiv = createInstrumentDiv(instrument);
+        cardColumns.appendChild(cardDiv);
+    }
+}
+
+createCards();
