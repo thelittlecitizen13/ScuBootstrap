445d5ad6418a8f27ec7151d98e2eb388c1c4e1e0
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-14 09:33:22 +0300

    added array of insruments

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+var fluteInstrument = {
+    description: "שם של חומוסייה",
+    id: 1,
+    imagePath: "Image/halil.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "חליל",
+    price: 1500,
+    typeID: 1,
+
+};
+var harmonicaInstrument = {
+    description: "מפוחית בטעם של פעם",
+    id: 2,
+    imagePath: "Image/mapuhit.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "מפוחית",
+    price: 50,
+    typeID: 1,
+
+};
+var tromboneInstrument = {
+    description: "סופר טרומבון",
+    id: 3,
+    imagePath: "Image/trombone.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "טרומבון",
+    price: 1000,
+    typeID: 1,
+
+};
+var trumpetInstrument = {
+    description: "חצוצרה מחשמלת",
+    id: 4,
+    imagePath: "Image/hazuzra.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "חצוצרה",
+    price: 35,
+    typeID: 1,
+
+};
+var violinInstrument = {
+    description: "סתם כינור (סקאם)",
+    id: 5,
+    imagePath: "",
+    instrumentOrders: null,
+    instrumentType: {id: 2, name: "פריטה"},
+    name: "כינור",
+    price: 500,
+    typeID: 2,
+
+};
+var clarinetInstrument = {
+    description: "הכלי של סקווידוויד",
+    id: 6,
+    imagePath: "Image/klarinet.png",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "קלרינט",
+    price: 2300,
+    typeID: 1,
+
+};
+
+var instruments = [fluteInstrument, harmonicaInstrument,tromboneInstrument, trumpetInstrument, clarinetInstrument, violinInstrument];
